1baf1f5e77b860914f7e840c0ed9db40777d788a
snlimadev <snlimadev@gmail.com>
2025-10-25 11:53:12 -0300

    Add rotation to meteors

--- scripts/Meteor.gd
+++ scripts/Meteor.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var vel = -400
+var rotation_speed = 15 if randi_range(0, 1) == 0 else -15
 @onready var scene = get_tree().current_scene
 
 func _ready():
@@ -9,6 +10,7 @@ func _ready():
 func _process(delta):
 	if scene.playingState:
 		set_position(get_position() + Vector2(vel * delta, 0))
+		rotation_degrees += rotation_speed * delta
 	else:
 		queue_free()
 	
